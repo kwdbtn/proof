c80d9c8f05122670aeaa0b490387de37576230bd
K <cadillacshine@hotmail.com>
2021-09-24 13:45:46 +0000

    Initial commit with models set up successfully

---
+++ app/Models/Candidate.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Candidate extends Model {
+    use HasFactory;
+
+    protected $fillable = ['first_name', 'last_name'];
+
+    public function election() {
+        return $this->belongsTo(Election::class);
+    }
+
+    public function polling_stations() {
+        return $this->belongsToMany(PollingStation::class);
+    }
+}
